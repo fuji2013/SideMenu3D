19275f2ddecd627656b33afc1dc7cd37f2f7727e
fuhi1983 <fuhi1983@gmail.com>
2015-04-27 12:26:48 +0900

    画面を構成するViewControllerファイルの追加

---
+++ SideMenu3D/ContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerViewController.swift
+//  SideMenu3D
+//
+//  Created by FUJISAWAHIROYUKI on 2015/04/27.
+//  Copyright (c) 2015年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
